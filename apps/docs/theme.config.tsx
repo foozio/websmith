3357ad0a3e0614053fbf101335d402ef8f77ba95
Nuzli Latief Hernawan <nuzlilatief@gmail.com>
2025-11-04 17:40:55 +0700

    Update all GitHub links to https://www.github.com/foozio/websmith

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,9 +1,9 @@
 export default {
   logo: <span>Websmith Kit</span>,
   project: {
-    link: 'https://github.com/websmith-kit/websmith-kit'
+    link: 'https://www.github.com/foozio/websmith'
   },
-  docsRepositoryBase: 'https://github.com/websmith-kit/websmith-kit/tree/main/apps/docs',
+  docsRepositoryBase: 'https://www.github.com/foozio/websmith/tree/main/apps/docs',
   footer: {
     text: 'Websmith Kit Docs'
   }
